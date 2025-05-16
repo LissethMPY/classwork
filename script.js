c15d73589a29093a4445cab9c762b9611f020a8a
Lisseth Pacheco <lmpacheco@uce.edu.ec>
2025-05-16 15:10:43 -0500

    Nuevo

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const servicios = {
   vercel: "https://vercel-ejemplo.vercel.app/",
   heroku: "https://flask-test-app-keylet-7cd2db8c273a.herokuapp.com/",
   digitalocean: "http://142.93.248.101/api/home",
+  openai: "https://platform.openai.com/",
+  gmail: "https://mail.google.com/"
 };
 
 function abrirServicio(nombre) {
